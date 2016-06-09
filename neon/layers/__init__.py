3e0eaf88a87928650d561fc7e9176c12f3bb5a13
Hugh Perkins <hughperkins@gmail.com>
2016-06-09 01:54:45 +0100

    remove Conv/compound layers

--- neon/layers/__init__.py
+++ neon/layers/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------
-from neon.layers.layer import (Conv, Convolution)
+from neon.layers.layer import (Convolution)
 #from neon.layers.recurrent import (Recurrent, LSTM, GRU, RecurrentSum, RecurrentMean, RecurrentLast,
 #                                   BiRNN, BiLSTM, DeepBiRNN, DeepBiLSTM)
 #from neon.layers.container import (Tree, Sequential, MergeMultistream, MergeBroadcast, Multicost,
