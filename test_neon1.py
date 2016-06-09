3ed040e17357965ead3f0ee43abb47eba450d294
Hugh Perkins <hughperkins@gmail.com>
2016-06-09 09:23:21 +0100

    ditch initializers

--- test_neon1.py
+++ test_neon1.py
@@ -1,5 +1,5 @@
 from neon.layers import Convolution
-from neon.initializers import Gaussian
+#from neon.initializers import Gaussian
 from neon.backends import gen_backend
 import numpy as np
 import pycuda.driver as cuda
@@ -15,9 +15,14 @@ output_filters = 32
 gen_backend(backend='gpu', batch_size=batch_size,
             datatype=np.float32, device_id=0)
 
-init = Gaussian()
+#init = Gaussian()
 
-conv = Convolution((3, 3, output_filters), strides=1, padding=1, init=init)
+conv = Convolution((3, 3, output_filters), strides=1, padding=1) #, init=init)
+print('created conv')
+W = np.random.randn(input_filters,3,3,output_filters).astype(np.float32)
+W_cuda = gpuarray.to_gpu(W)
+conv.W = W_cuda
+#conv.W = np.random.randn(input_filters,3,3,output_filters).astype(np.float32)
 
 #inputs = np.zeros((batch_size,image_size, image_size,input_filters), dtype=np.float32)
 inputs = np.zeros((input_filters,image_size, image_size,batch_size), dtype=np.float32)
