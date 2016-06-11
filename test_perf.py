fdd0c58a8566d82d851b84d45b360ce6424a6236
Hugh Perkins <hughperkins@gmail.com>
2016-06-11 13:38:42 +0100

    migrate test_perf to use Backend contextmanager

--- test_perf.py
+++ test_perf.py
@@ -1,6 +1,5 @@
-from neon.layers import Convolution
-#from neon.initializers import Gaussian
-from neon.backends import gen_backend
+from neon.layers.layer import Convolution
+from neon.backends import Backend
 import numpy as np
 import pycuda.driver as cuda
 #import pycuda.autoinit
@@ -14,51 +13,40 @@ output_filters = 32
 
 np.random.seed(123)
 
-gen_backend(batch_size=batch_size,
-            datatype=np.float32, device_id=0)
+with Backend(batch_size=batch_size,
+            datatype=np.float32, device_id=0) as be:
+    conv = Convolution((3, 3, output_filters), strides=1, padding=1, be=be)
+    print('created conv')
+    W = np.random.randn(input_filters,3,3,output_filters).astype(np.float32)
+    W_cuda = gpuarray.to_gpu(W)
+    conv.W = W_cuda
 
-#init = Gaussian()
+    print('type(W_cuda)', type(W_cuda))
 
-conv = Convolution((3, 3, output_filters), strides=1, padding=1) #, init=init)
-print('created conv')
-W = np.random.randn(input_filters,3,3,output_filters).astype(np.float32)
-W_cuda = gpuarray.to_gpu(W)
-conv.W = W_cuda
+    inputs = np.zeros((input_filters,image_size, image_size,batch_size), dtype=np.float32)
+    inputs[:] = np.random.randn(*inputs.shape)
+    inputs_cuda = gpuarray.to_gpu(inputs)
 
-print('type(W_cuda)', type(W_cuda))
+    print('type(inputs_cuda)', type(inputs_cuda))
 
-#conv.W = np.random.randn(input_filters,3,3,output_filters).astype(np.float32)
-
-#inputs = np.zeros((batch_size,image_size, image_size,input_filters), dtype=np.float32)
-inputs = np.zeros((input_filters,image_size, image_size,batch_size), dtype=np.float32)
-inputs[:] = np.random.randn(*inputs.shape)
-inputs_cuda = gpuarray.to_gpu(inputs)
-
-print('type(inputs_cuda)', type(inputs_cuda))
-
-conv.configure((input_filters,image_size, image_size))
-print('configure done')
-#conv.allocate()
-#print('conv.outputs.shape', conv.outputs.shape)
-#print('type(conv.outputs)', type(conv.outputs))
-#print('type(conv.outputs.gpudata)', type(conv.outputs.gpudata))
-#print('allocate done')
-outputs = np.zeros((image_size * image_size * output_filters, batch_size), dtype=np.float32)
-outputs_cuda = gpuarray.to_gpu(outputs)
-conv.outputs = outputs_cuda
-conv.fprop(inputs_cuda)
-for it in range(3):
-  start = time.time()
-  for i in range(10):
+    conv.configure((input_filters,image_size, image_size))
+    print('configure done')
+    outputs = np.zeros((image_size * image_size * output_filters, batch_size), dtype=np.float32)
+    outputs_cuda = gpuarray.to_gpu(outputs)
+    conv.outputs = outputs_cuda
     conv.fprop(inputs_cuda)
-  cuda.Context.synchronize()
-  print('time=', time.time() - start)
+    for it in range(3):
+      start = time.time()
+      for i in range(10):
+        conv.fprop(inputs_cuda)
+      cuda.Context.synchronize()
+      print('time=', time.time() - start)
 
 
-outputs = outputs_cuda.get()
-print(outputs[1:3,1:3])
+    outputs = outputs_cuda.get()
+    print(outputs[1:3,1:3])
 
-assert abs(outputs[1,1] - 1.33960593) < 1e-4
-assert abs(outputs[1,2] + 6.06682396) < 1e-4
-assert abs(outputs[2,2] - 8.76905346) < 1e-4
+    assert abs(outputs[1,1] - 1.33960593) < 1e-4
+    assert abs(outputs[1,2] + 6.06682396) < 1e-4
+    assert abs(outputs[2,2] - 8.76905346) < 1e-4
 
