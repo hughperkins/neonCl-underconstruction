0b632af13bba2a82564bff7eafd7bd24d9ad81c2
Hugh Perkins <hughperkins@gmail.com>
2016-06-13 08:52:58 +0800

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# tested (somewhat...) on ubuntu 16.04
+
+sudo apt-get install python3 python3-dev opencl-headers python-virtualenv
+virtualenv -p python3 env3
+source env3/bin/activate
+pip install -U pip
+pip install -U setuptools
+pip install -U wheel
+pip install -r requirements.txt
+
